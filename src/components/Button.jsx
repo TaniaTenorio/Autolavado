c250ab472e856df4581bb4f2c17cf2358df0d795
TaniaTenorio <taniatenorio@hotmail.com>
2019-11-22 10:58:37 -0600

    Adding button to map view

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,14 @@ import './Components.css';
 class Button extends Component {
 
     render() {
-    return <button className="waves-effect waves-light btn-small" type="submit" onClick={this.props.submit}>{this.props.text}</button>
+    return (<button 
+        className="waves-effect waves-light btn-small" 
+        type="submit" 
+        onClick={this.props.submit}
+        >
+            {this.props.text}
+        </button>
+        )   
     }
 };
 
