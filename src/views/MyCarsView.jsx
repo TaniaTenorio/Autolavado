9d7ca67dc8a19ef13abe4a57094d70ba5fb6fa5d
SolGee <solsv00@hotmail.com>
2019-11-22 14:25:16 -0600

    Create followwasher view

--- src/views/MyCarsView.jsx
+++ src/views/MyCarsView.jsx
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import Button from '../components/Button';
 
-
 class MyCarsView extends Component {
-    render() {
-        return(
-            <section>
-                <article>
-                    <h1>Mis autos</h1>
-                    <p>Recuerda que puedes registrar máximo 5 autos</p>
-                </article>
-                <article>
-                    <ul>
-                        <li>Volkswage</li>
-                        <li>Bora</li>
-                        <li>Negro</li>
-                        <li>Placas: ***AAA</li>
-                    </ul>
-                </article>
-                <Button text={"Agregar"} />
-            </section>
-        )
-    }
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      Marca: '',
+      Modelo: '',
+      Placas: '',
+      Color: ''
+    };
+  }
+
+  componentDidMount() {
+    //Set localstorage
+    const { Marca, Modelo, Placas, Color } = JSON.parse(
+      localStorage.getItem('clients')
+    );
+    this.setState({ Marca, Modelo, Placas, Color });
+  }
+  render() {
+    return (
+      <section>
+        <Link to='/follow'>
+          <a href=''>
+            <i className='material-icons'>chevron_left</i>
+          </a>
+        </Link>
+        <article>
+          <h1>Mis autos</h1>
+          <p>Recuerda que puedes registrar máximo 5 autos</p>
+        </article>
+        <article>
+          <ul>
+            <li>{this.state.Marca}</li>
+            <li>{this.state.Modelo}</li>
+            <li>{this.state.Color}</li>
+            <li>{this.state.Placas}</li>
+          </ul>
+        </article>
+        <Button text={'Agregar'} />
+      </section>
+    );
+  }
+}
 
 export default MyCarsView;
