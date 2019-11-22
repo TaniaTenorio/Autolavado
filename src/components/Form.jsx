bf417226ffa7355835e25b91befe51732743b5b2
TaniaTenorio <taniatenorio@hotmail.com>
2019-11-22 11:24:31 -0600

    changing state tu register view for form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,36 +6,33 @@ import './Components.css';
 import Button from './Button';
 
 class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      phoneNumber: '',
-      password: ''
-    };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+      
+//     };
+//   }
 
-  onChangeHandler = e => {
-    //console.log(e.target.value);
-    this.setState({
-      [e.target.name]: e.target.value
-      //[e.target.number]: e.target.value
-    });
-  };
+//   onChangeHandler = e => {
+//     //console.log(e.target.value);
+//     this.setState({
+//       [e.target.name]: e.target.value
+//       //[e.target.number]: e.target.value
+//     });
+//   };
 
-  onSubmit = e => {
-    e.preventDefault();
-    localStorage.setItem('clientes', JSON.stringify(this.state));
-    this.props.history.push('/home');
-    console.log(JSON.parse(localStorage.getItem('clientes')));
-    //this.props.agregarClientes(this.state.name);
-  };
+//   onSubmit = e => {
+//     e.preventDefault();
+//     localStorage.setItem('clientes', JSON.stringify(this.state));
+//     this.props.history.push('/home');
+//     console.log(JSON.parse(localStorage.getItem('clientes')));
+//     //this.props.agregarClientes(this.state.name);
+//   };
 
   render() {
     return (
       <section>
-        <h3>Registro</h3>
+        <h3>{this.props.title}</h3>
         <form>
           <fieldset>
             <label name='text'>
@@ -43,40 +40,40 @@ class Form extends Component {
                 name='name'
                 type='text'
                 placeholder={this.props.text1}
-                value={this.state.name}
-                onChange={this.onChangeHandler}></input>
+                value={this.props.name}
+                onChange={this.props.onChangeHandler}></input>
             </label>
             <label name='phone-number'>
               <input
                 name='phoneNumber'
                 type='text'
                 placeholder={this.props.text2}
-                value={this.state.phone}
-                onChange={this.onChangeHandler}></input>
+                value={this.props.phone}
+                onChange={this.props.onChangeHandler}></input>
             </label>
             <label name='email'>
               <input
                 name='email'
                 type='email'
                 placeholder={this.props.text3}
-                value={this.state.email}
-                onChange={this.onChangeHandler}></input>
+                value={this.props.email}
+                onChange={this.props.onChangeHandler}></input>
             </label>
             <label>
               <input
                 name='password'
                 type='password'
                 placeholder={this.props.text4}
-                value={this.state.password}
-                onChange={this.onChangeHandler}></input>
+                value={this.props.password}
+                onChange={this.props.onChangeHandler}></input>
             </label>
             <label>
               <input
                 name='password'
                 type='password'
                 placeholder='Confirmar contraseña'
-                value={this.state.password}
-                onChange={this.onChangeHandler}></input>
+                value={this.props.password}
+                onChange={this.props.onChangeHandler}></input>
             </label>
             <div className='checkbox-label'>
               <label>
@@ -84,7 +81,7 @@ class Form extends Component {
                 <span>Acepto términos y condiciones</span>
               </label>
               <br />
-              <Button text={'Siguiente'} submit={this.onSubmit} />
+              <Button text={'Siguiente'} submit={this.props.onSubmitHandler} />
             </div>
           </fieldset>
         </form>
